9750ffa5341f113230338d6975bb681c1094f95a
Peyton Kellogg <pekellogg@hotmail.com>
2022-06-01 16:31:33 -0700

    use lesson as template for phase 3 project queries

--- Gemfile
+++ Gemfile
@@ -2,8 +2,11 @@ source 'https://rubygems.org'
 
 gem 'rails', '5.0.7.1'
 gem 'sqlite3', '~>1.3.6'
+# gem "sqlite3", '~> 1.4' # PK updated gem version in order to leverage...
 
 
 group :development, :test do
   gem "rspec-rails"
+  gem "pry"
+  gem "byebug"
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,23 +2,56 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
-    # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
 
   def self.motorboat_operators
-    # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
 
   def self.talented_seafarers
-    # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
-    # where.not("id IN (?)", self.sailors.pluck(:id))
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 
 end
+
+# SCHEMA/RELATIONSHIPS REF
+# create_table "captains"
+#   t.string   "name"
+#   t.boolean  "admiral"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   has_many :boats
+
+# create_table "boats"
+#   t.string   "name"
+#   t.integer  "length"
+#   t.integer  "captain_id"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   belongs_to  :captain
+#   has_many    :classifications, through: :boat_classifications
+#   has_many    :boat_classifications
+
+# create_table "boat_classifications"
+#   t.integer  "boat_id"
+#   t.integer  "classification_id"
+#   t.datetime "created_at",        null: false
+#   t.datetime "updated_at",        null: false
+#   belongs_to :boat
+#   belongs_to :classification
+
+# create_table "classifications"
+#   t.string   "name"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   has_many :boats, through: :boat_classifications
+#   has_many :boat_classifications
